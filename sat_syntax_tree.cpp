88a3bf623f9500e4660cbddf7fe8f47547ff7151
Justin Hinckley <justin.h.hinckley@gmail.com>
2023-11-26 19:43:30 -0500

    b

--- sat_syntax_tree.cpp
+++ sat_syntax_tree.cpp
@@ -165,6 +165,8 @@ int main()
     SAT_Expression expression_combined2(Operator::AND, new SAT_Expression(0), expression2);
     SAT_Expression *expression3 = new SAT_Expression(Operator::AND, new SAT_Expression(0), new SAT_Expression(1));
     SAT_Expression expression_combined3(Operator::AND, expression3, new SAT_Expression(2));
+    SAT_Expression *expression3_duplicate = new SAT_Expression(Operator::AND, new SAT_Expression(0), new SAT_Expression(1));
+    SAT_Expression expression_combined3_duplicate(Operator::AND, expression3_duplicate, new SAT_Expression(2));
 
     expressionMap[expression1] = 1;
     expressionMap[expression_combined2] = 2;
